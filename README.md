8451fa4a05d179272fddee6fd5e1e23fc474d083
Felix Morgner <felix.morgner@gmail.com>
2016-07-23 16:53:55 +0200

    Add naïve counter implementation
    This implementation should not be considered to be especially good. It
    is a minimalistic busy spinning implementation.
    
    The ESP8266 does not allow for long running tasks, especially IO tasks,
    in timer callbacks.  For this reason, the programme is designed as a
    busy loop with the only timer callback being the read_switches function.
    This is NOT energetically optimal, but it works.
    
    I propose that deep-sleeping and updating via pin-toggle interrupts
    should be implemented in a future release.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ferraris-enumerator
-Counts People now Present for SpaceAPI
+
+Counts people in the hacker space and publishes the result on a SpaceAPI server.
+
+## Configuration
+
+You need to create a file called `config.h` in the same directory as `ferraris-counter.ino` containing the follwing information:
+
+| Field name     | Field type           | Meaning                                                  |
+----------------------------------------------------------------------------------------------------
+| `kSSID`        | `char const * const` | Your networks **SSID**                                   |
+| `kPassword`    | `char const * const` | Your networks **Password**                               |
+| `kHost`        | `char const * const` | The **hostname** of the target SpaceAPI server           |
+| `kPort`        | `int const`          | The **port** your SpaceAPI server is listening on        |
+| `kPath`        | `char const * const` | The **path** for the PUT request on your SpaceAPI server |
+| `kField`       | `char const * const` | The **key** of the field transmitted in the PUT request  |
+| `kFingerprint` | `char const * const` | Your SpaceAPI servers **TLS fingerprint**                |
+
+For the sake of simplicity, you can of course define these variables using `auto constexpr`. You should make sure to **_NEVER_** add this configuration file to your repository since this would compromise your network security.
+
+## Platform
+
+This code is designed to run on an ESP8266 deelopment board using 8 switches and a two-wire seven-segment display. The code was written using the ESP8266 platform plugin for Arduino.
---
+++ ferraris-enumerator.ino
@@ -0,0 +1,132 @@
+/*
+
+Copyright (c) 2016, Felix Morgner
+Copyright (c) 2016, Hackerspace Coredump Rapperswil
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+ * Neither the name of Coredump nor the names of its contributors may be
+   used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "config.h"
+
+#include "TM1637.h"
+#include <Ticker.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+#include <cstdint>
+
+auto gDisplay = tm1637{D0, D1};
+auto gUpdater = Ticker{};
+auto gPeople = 0u;
+auto gShouldPost = true;
+auto gClient = WiFiClientSecure{};
+
+std::uint8_t switches[] = {D2, D3, D4, D5, D6, D7, D9, D10};
+
+void setup()
+  {
+  WiFi.begin(kSSID, kPassword);
+
+  while(WiFi.status() != WL_CONNECTED)
+    {
+    delay(500);
+    }
+
+  for(auto const sw : switches)
+    {
+    pinMode(sw, INPUT_PULLUP);
+    digitalWrite(sw, HIGH);
+    }
+
+  gDisplay.power(true);
+  gDisplay.setBrightness(6);
+  gDisplay.clear();
+  gUpdater.attach(0.05f, update);
+
+  post();
+  }
+
+void loop()
+  {
+  if(gShouldPost)
+    {
+    gShouldPost = false;
+    post();
+    }
+  }
+
+unsigned read_switches()
+  {
+  auto people = 0u;
+
+  for(auto const sw : switches)
+    {
+    people <<= 1;
+    people += !digitalRead(sw);
+    }
+
+  return people;
+  }
+
+void update()
+  {
+  auto const people = read_switches();
+  auto static it = 0;
+
+  if(gPeople != people)
+    {
+    gPeople = people;
+
+    if(gPeople)
+      {
+      gDisplay.writeInteger(gPeople);
+      }
+    else
+      {
+      gDisplay.clear();
+      }
+
+    gShouldPost = true;
+    }
+
+  }
+
+void post()
+  {
+  auto value = String("value=") + gPeople;
+  auto request = String("PUT ") + kPath + " HTTP/1.1\r\n" +
+                 "Host: " + kHost + "\r\n" +
+                 "Content-type: application/x-www-form-urlencoded\r\n" +
+                 "Content-length: " + value.length() + "\r\n" +
+                 "\r\n" + value + "\r\n";
+
+  if(!gClient.connect(kHost, kPort) || !gClient.print(request))
+    {
+    gShouldPost = true;
+    }
+  }
+
